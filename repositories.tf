97decb32fa51382f19b4f79eb5fa232c14bc5b70
makiton <makiton@gmail.com>
2021-08-25 23:13:07 +0900

    terraform fmt

--- repositories.tf
+++ repositories.tf
@@ -1,12 +1,12 @@
 resource "github_repository" "tf_github" {
-  name = "tf_github"
+  name        = "tf_github"
   description = "my github repositories"
 
-  allow_squash_merge = false
-  allow_rebase_merge = false
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
   delete_branch_on_merge = true
 
-  auto_init = true
+  auto_init          = true
   gitignore_template = "Terraform"
 
   vulnerability_alerts = true
