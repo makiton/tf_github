99026ae3459b6bfa3de72cf747cfceb2dbcc4eba
makiton <makiton@gmail.com>
2021-12-30 11:22:22 +0900

    Refactor repositories

--- repositories.tf
+++ repositories.tf
@@ -1,60 +1,3 @@
-resource "github_repository" "tf_github" {
-  name        = "tf_github"
-  description = "my github repositories"
-
-  allow_squash_merge     = false
-  allow_rebase_merge     = false
-  delete_branch_on_merge = true
-
-  auto_init          = true
-  gitignore_template = "Terraform"
-
-  vulnerability_alerts = true
-}
-
-resource "github_repository" "paiza" {
-  name        = "paiza"
-  description = "my answers for paiza problems"
-
-  visibility = "private"
-
-  allow_squash_merge     = false
-  allow_rebase_merge     = false
-  delete_branch_on_merge = true
-
-  auto_init          = true
-  gitignore_template = "Python"
-
-  vulnerability_alerts = true
-}
-
-resource "github_repository" "atcoder" {
-  name        = "atcoder"
-  description = "my answers for atcoder problems"
-
-  allow_squash_merge     = false
-  allow_rebase_merge     = false
-  delete_branch_on_merge = true
-
-  auto_init          = true
-  gitignore_template = "Python"
-
-  vulnerability_alerts = true
-}
-
-resource "github_repository" "setup" {
-  name        = "setup"
-  description = "setup scripts for my PCs"
-
-  allow_squash_merge     = false
-  allow_rebase_merge     = false
-  delete_branch_on_merge = true
-
-  auto_init = true
-
-  vulnerability_alerts = true
-}
-
 locals {
   repositories = {
     "template-nextjs" : {
@@ -71,6 +14,23 @@ locals {
       "gitignore_template" : "Ruby"
       "is_template" : true
     }
+    "tf_github" : {
+      "description" : "my github repositories"
+      "gitignore_template" : "Terraform"
+
+    }
+    "paiza" : {
+      "description" : "my answers for paiza problems"
+      "gitignore_template" : "Python"
+      "private" : true
+    }
+    "atcoder" : {
+      "description" : "my answers for atcoder problems"
+      "gitignore_template" : "Python"
+    }
+    "setup" : {
+      "description" : "setup scripts for my PCs"
+    }
   }
 }
 
@@ -78,6 +38,8 @@ resource "github_repository" "repositories" {
   for_each    = local.repositories
   name        = each.key
   description = each.value.description
+
+  visibility = lookup(each.value, "private", false) ? "private" : "public"
 
   is_template = lookup(each.value, "is_template", false)
 
